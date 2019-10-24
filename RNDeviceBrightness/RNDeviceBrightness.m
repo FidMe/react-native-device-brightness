82e5077de8e393d119ef113428bc7815485ccd57
Romain OURGORRY <rourgorry@snapp.fr>
2019-10-24 11:42:36 +0200

    Fix iOS thread exception for iOS 13

--- RNDeviceBrightness/RNDeviceBrightness.m
+++ RNDeviceBrightness/RNDeviceBrightness.m
@@ -14,14 +14,18 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(setBrightnessLevel:(float)brightnessLevel)
 {
-    [UIScreen mainScreen].brightness = brightnessLevel;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [UIScreen mainScreen].brightness = brightnessLevel;
+    });
 }
 
 RCT_REMAP_METHOD(getBrightnessLevel,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    resolve(@([UIScreen mainScreen].brightness));
+    dispatch_async(dispatch_get_main_queue(), ^{
+        resolve(@([UIScreen mainScreen].brightness));
+    });
 }
 
 @end
